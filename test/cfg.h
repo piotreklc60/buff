ef5ab5f4b4bd631d236b4fead28a627a6ad6b7f8
piotreklc60 <piotreklc60@gmail.com>
2019-03-19 21:09:15 +0100

    Update cfg.h

--- test/cfg.h
+++ test/cfg.h
@@ -37,45 +37,5 @@ typedef uint64_t ptrdiff_t;
 #define bool_t uint8_t
 #define false 0
 #define true 1
-// #define size_t int
-
-/**
- * IOTERM_CMD_GROUP_START
- * IOTERM_CMD_GROUP_END
- * IOTERM_CMD_PARAM_STRING
- * IOTERM_CMD_PARAM_BOOL
- * IOTERM_CMD_PARAM_UINT
- * IOTERM_CMD_PARAM_INT
- * IOTERM_CMD_PARAM_UDATA
- * IOTERM_CMD_PARAM_DATA
- * IOTERM_CMD_PARAM_LAST
- */
-
-#define IOTERM_CMD_XMACRO_HELP_SUPPORT          IOTERM_FEATURE_SUPPORTED
-
-#define USBD_CMD() \
-IOTERM_CMD_PARAM_STRING(         "",                     test_cmd_empty,             "simple function which prints empty cmd") \
-IOTERM_CMD_PARAM_STRING(         "file",                 test_cmd_file,              "simple function which prints file name") \
-IOTERM_CMD_PARAM_STRING(         "line",                 test_cmd_line,              "simple function which prints line number") \
-IOTERM_CMD_GROUP_START(          "usb",                                              "group of commands for USBD library printing") \
-    IOTERM_CMD_PARAM_STRING(         "info",             test_cmd_usb_info,          "general information about USBD library") \
-    IOTERM_CMD_GROUP_START(          "dev",                                          "group of commands for USBD DEV sub-module") \
-        IOTERM_CMD_PARAM_STRING(         "state",        test_cmd_usb_state,         "gets state of USBD") \
-        IOTERM_CMD_PARAM_STRING(         "activate",     test_cmd_usb_activate,      "actvates USBD") \
-        IOTERM_CMD_PARAM_STRING(         "deactivate",   test_cmd_usb_deactivate,    "deactvates USBD") \
-        IOTERM_CMD_GROUP_START(          "ep",                                       "group of EP-related commands for USBD DEV sub-module") \
-            IOTERM_CMD_PARAM_STRING(         "",         test_cmd_usb_ep,            "general cmd for ep") \
-            IOTERM_CMD_PARAM_UDATA_DEC(      0,          test_cmd_usb_ep0_print,     "gets state of endpoint 0") \
-            IOTERM_CMD_PARAM_UINT_DEC(                   test_cmd_usb_ep_print,      "gets state of specific endpoint") \
-            IOTERM_CMD_PARAM_STRING(         "overview", test_cmd_usb_ep_overview,   "gets state of all endpoints") \
-        IOTERM_CMD_GROUP_END() \
-    IOTERM_CMD_GROUP_END() \
-    IOTERM_CMD_PARAM_STRING(         "io",               test_cmd_usb_io,            "gets state of IO") \
-IOTERM_CMD_GROUP_END() \
-IOTERM_CMD_PARAM_SHORT_HELP(     "help",                                             "prints short help") \
-IOTERM_CMD_PARAM_FULL_HELP(      "man",                                              "prints full help") \
-IOTERM_CMD_PARAM_STRING(         "size",                 test_cmd_size,              "prints size of structure \"CMD_XMACRO_record_XT\"") \
-IOTERM_CMD_PARAM_STRING(         "sizeof",               test_cmd_sizeof,            "prints size of \"cmd\" structure") \
-IOTERM_CMD_PARAM_LAST()
 
 #endif
